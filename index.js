4b52ae8332ff81561ae27f39073552376a585eba
Njire Njeri <njirenjeri@gmail.com>
2024-11-25 22:50:20 +0300

    working with strings

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+
+let currentUser = "Jimmy Jay";
+const welcomeMessage = 'Welcome to Flatbook,  ' + currentUser + '!';
+// let currentUser = "JIMMY JAY";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = "Welcome, " + currentUser.slice(0, 1) + " !";
